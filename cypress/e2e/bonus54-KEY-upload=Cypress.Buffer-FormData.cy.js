837ede53d94857ebea9c1034d1e7462058caeed6
Murat Ozcan <muratkerem@gmail.com>
2022-12-10 16:16:19 -0600

    54 done

--- cypress/e2e/bonus54-KEY-upload=Cypress.Buffer-FormData.cy.js
+++ cypress/e2e/bonus54-KEY-upload=Cypress.Buffer-FormData.cy.js
@@ -36,42 +36,44 @@ it('uploads a JSON file using a cy.request', () => {
   // convert it to JSON and create a Blob
   // using Cypress.Blob.createBlob method
   // Tip: the 2nd argument should be "application/json"
-  cy.fixture('apple.json', 'application/json')
-    .then((appleJson) => {
-      console.log(appleJson)
-      return Cypress.Blob.binaryStringToBlob(appleJson, 'application/json')
+  cy.fixture('apple.json')
+    .then(JSON.stringify)
+    .then((text) => {
+      // blog needs to take an array as argument
+      console.log([text])
+      return Cypress.Blob.createBlob([text], 'application/json')
     })
-    .then((blob) => {
-      // set the blob in the form under the form field name
-      // Tip: don't forget to give the filename
-      console.log(blob)
-      formData.set('form', blob, 'apple.json')
-    })
-    .then(() => {
-      console.log(formData)
-    })
+    .then((blob) => formData.set('form', blob, 'apple.json'))
   //
   //
   // post the form to "/upload-json-file" endpoint
   // https://on.cypress.io/request
+  // grab the response body
+  // https://on.cypress.io/its
+  // and convert it from ArrayBuffer to Buffer
+  // convert the buffer to a string
+  // https://on.cypress.io/invoke
+  // and write the string to the current HTML document
+  // https://on.cypress.io/document
   cy.request({
     method: 'POST',
     url: '/upload-json-file',
     body: formData
   })
-  //
-  // grab the response body
-  // https://on.cypress.io/its
-  //
-  // and convert it from ArrayBuffer to Buffer
-  //
-  // convert the buffer to a string
-  // https://on.cypress.io/invoke
-  //
-  // and write the string to the current HTML document
-  // https://on.cypress.io/document
-  //
+    .its('body')
+    .then(Cypress.Buffer.from)
+    .invoke('toString', 'utf8')
+    .then((b) => {
+      console.log(b)
+    })
+    .then((html) => cy.document().invoke('write', html))
+
   // confirm the server has received out JSON file
   // by checking the response HTML elements:
   // the page heading, the filename, the JSON output
+  cy.fixture('apple.json').then((appleJson) => {
+    cy.contains('h2', 'Uploaded JSON file')
+    cy.contains('p', appleJson)
+    cy.get('pre').should('contain', appleJson.fruit)
+  })
 })
